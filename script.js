f39e41d23c0cc02fe506a9cffa1761788c5d27e8
ngkhoi <115385897+ngyykhoi@users.noreply.github.com>
2025-03-10 08:50:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// let text = document.querySelector(".subtitle");
+// let a = 1;
+// console.log(text.textContent);
+
+// function textShuffle() {
+//   text.style.opacity = 0;
+//   setTimeout(() => {
+//     text.textContent = "Định hướng nghề nghiệp tương lai";
+//     text.style.opacity = 1;
+//   }, 1000);
+
+//   setTimeout(function () {
+//     text.style.opacity = 0;
+//   }, 5000);
+
+//   setTimeout(() => {
+//     text.textContent = "Khám phá tính cách bản thân";
+//     text.style.opacity = 1;
+//   }, 6000);
+// }
+
+// setInterval(textShuffle, 5000);
+// while (a > 0) {
+//   console.log("Hello World");
+//   sleep(1000);
+// }
